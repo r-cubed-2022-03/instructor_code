c75ec8334ce0910322335c7ea24e28bbb75bb3ec
andersaskeland <anders.askeland@gmail.com>
2022-03-01 15:45:55 +0100

    Added readme file

---
+++ R/project-session.R
@@ -0,0 +1,16 @@
+# Just stuff
+weight_kilos <- 100
+weight_kilos
+
+head(airquality)
+
+colnames(airquality)
+
+str(airquality)
+
+summary(airquality)
+
+# Source stuff
+source(here::here("R/package-loading.R"))
+
+
