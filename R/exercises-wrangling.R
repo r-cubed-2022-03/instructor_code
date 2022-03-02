cef1c85b6a4531b20577a1b0727ffe626b504aa9
Stine Hedegaard Scheuer <stine.scheuer@regionh.dk>
2022-03-02 13:14:06 +0100

    Exersice 6.9

--- R/exercises-wrangling.R
+++ R/exercises-wrangling.R
@@ -13,3 +13,24 @@ summary(NHANES)
 
 # Look over the dataset documentation
 ?NHANES
+
+
+# 6.9
+
+# 1. Select specific columns
+nhanes_small %>%
+    select(bp_sys_ave, education)
+
+# 2. Rename columns
+nhanes_small %>%
+    rename(bp_sys = bp_sys_ave,
+           bp_dia = bp_dia_ave)
+
+# 3. Re-write with pipe
+nhanes_small %>%
+    select(bmi, contains("age"))
+
+# 4. Re-write with pipe
+nhanes_small %>%
+    select(starts_with("bp_")) %>%
+    rename(bp_systolic = bp_sys_ave)
